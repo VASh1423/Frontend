fd58577646ecd834128d185957dfbf1d6e4f9499
VASh <shokarev.vl@gmail.com>
2021-02-26 13:25:29 +0300

    updated mern

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { useRoutes } from "./routes";
 import { useAuth } from "./hooks/auth.hook";
 import { AuthContext } from "./context/AuthContext";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   const {token, login, logout, userId} = useAuth()
@@ -14,6 +15,7 @@ function App() {
       token, login, logout, userId, isAuthentication
     }}>
       <Router>
+        {isAuthentication && <Navbar/>}
         <div className="container">
           {routers}
         </div>
