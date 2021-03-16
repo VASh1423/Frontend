cd446e57814152e80978113a12f2ba4dc91eaa10
VASh <shokarev.vl@gmail.com>
2021-03-16 22:01:17 +0300

    study express

---
+++ study-express/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import path from 'path'
+import {requestTime, logger} from './middlewares.js'
+import serverRoutes from './routes/servers.js'
+
+const __dirname = path.resolve()
+const PORT = process.env.PORT ?? 3000
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'ejs'))
+
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(requestTime)
+app.use(logger)
+
+app.use(serverRoutes)
+
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Main page', active: 'main'})
+})
+
+app.get('/features', (req, res) => {
+  res.render('features', {title: 'Features page', active: 'features'})
+})
+
+// app.get('/', (req, res) => {
+//   // res.send('<h1>Hello world</h1>')
+//   res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+// })
+
+// app.get('/download', (req, res) => {
+//   res.download(path.resolve(__dirname, 'static', 'index.html'))
+// })
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT}...`)
+})
