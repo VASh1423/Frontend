aab447458ff03e0e7d6dd5263190aa2c67a9c725
VASh <shokarev.vl@gmail.com>
2021-10-14 14:47:29 +0300

    updated

--- mern-cloud-disk/server/routes/auth.routes.js
+++ mern-cloud-disk/server/routes/auth.routes.js
@@ -1,8 +1,11 @@
 const Router = require('express')
 const User = require('../models/User')
-const router = Router()
 const bcrypt = require('bcrypt')
 const {check, validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+const router = Router()
 
 router.post('/registration',
   [
@@ -25,13 +28,47 @@ router.post('/registration',
       return res.status(400).json({message: `User with email ${email} already exist`})
     }
 
-    const hashPassword = await bcrypt.hash(password, 15)
+    const hashPassword = await bcrypt.hash(password, 8)
 
     const user = new User({email, password: hashPassword})
 
     await user.save()
 
     return res.json({message: 'User was created'})
+
+  } catch (error) {
+    console.log(error)
+    res.send({message: 'Server error'})
+  }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+
+    if(!user){
+      return res.status(404).json({message: 'User not found'})
+    }
+
+    const isPassValid = bcrypt.compareSync(password, user.password)
+
+    if(!isPassValid){
+      return res.status(400).json({message: 'Invalid password'})
+    }
+
+    const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: '1h'})
+
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar
+      }
+    })
 
   } catch (error) {
     console.log(error)
