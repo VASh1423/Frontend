4706c1289443c873408f74458d95af8cd13b8c4f
VASh <shokarev.vl@gmail.com>
2021-11-12 14:22:38 +0300

    updated

---
+++ backend-course/PostController.js
@@ -0,0 +1,52 @@
+import Post from "./Post.js"
+import PostService from "./PostService.js"
+
+class PostController{
+  async create(req, res){
+    try {
+      console.log(req.files);
+      const post = await PostService.create(req.body)
+      res.status(200).json(post)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+
+  async getAll(req, res){
+    try {
+      const posts = await PostService.getOne()
+      return res.json(posts)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+
+  async getOne(req, res){
+    try {
+      const post = await PostService.getOne(req.params.id)
+      return res.json(post)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+
+  async update(req, res){
+    try {
+      const updatedPost = await PostService.update(req.body)
+      return res.json(updatedPost)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+
+  async delete(req, res){
+    try {
+      const post = await PostService.delete(req.params.id)
+      return res.json(post)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+}
+
+export default new PostController()
--- backend-course/index.js
+++ backend-course/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from 'mongoose'
 import router from './router.js'
+import fileUpload from "express-fileupload"
 
 const PORT = 5000
 const DB_URL = `mongodb+srv://admin:admin@cluster0.b8jep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
@@ -8,6 +9,7 @@ const DB_URL = `mongodb+srv://admin:admin@cluster0.b8jep.mongodb.net/myFirstData
 const app = express()
 
 app.use(express.json())
+app.use(fileUpload())
 
 app.use('/api', router)
 
