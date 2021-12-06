a9872d6c95d3d7c12e94d61fc22a28090c54b05b
VASh <shokarev.vl@gmail.com>
2021-12-06 15:40:00 +0300

    updated

--- real-time-course/client/src/App.jsx
+++ real-time-course/client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import LongPulling from './LongPulling';
+import EventSourcing from './EventSourcing';
+import WebSock from './WebSock';
 
 function App() {
   return (
     <div>
-      <LongPulling/>
+      <WebSock/>
     </div>
   );
 }
