5e81ccc15c614e523a976f18b94988b0abdabe82
VASh <shokarev.vl@gmail.com>
2021-12-13 16:14:32 +0300

    updated

--- nest-course/src/roles/roles.model.ts
+++ nest-course/src/roles/roles.model.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
+import { User } from "src/users/users.model";
+import { UserRoles } from "./user-roles.module";
 
 interface RoleCreateAttrs{
   value: string;
@@ -19,4 +21,7 @@ export class Role extends Model<Role, RoleCreateAttrs>{
   @ApiProperty({example:'Администратор', description:'Описание роли'})
   @Column({type: DataType.STRING, allowNull: false})
   description: string;
+
+  @BelongsToMany(() => User, () => UserRoles)
+  users: User[];
 }
