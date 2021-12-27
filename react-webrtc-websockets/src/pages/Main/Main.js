c546a69739697a2c83d7c87ccd02a6482e7bd55b
VASh <shokarev.vl@gmail.com>
2021-12-27 15:49:09 +0300

    updated

--- react-webrtc-websockets/src/pages/Main/Main.js
+++ react-webrtc-websockets/src/pages/Main/Main.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import socket from '../../socket'
+import ACTIONS from '../../socket/actions'
+import { useNavigate } from 'react-router-dom'
+import { v4 } from 'uuid'
 
 export default function Main() {
+  const history = useNavigate()
+  const [rooms, updateRooms] = useState([])
+
+  useEffect(() => {
+    socket.on(ACTIONS.SHARE_ROOMS, ({rooms = []}) => {
+      updateRooms(rooms)
+    })
+  }, [])
+
   return (
     <div>
-      Main page
+      <h1>
+        Available Rooms
+      </h1>
+      <ul>
+        {rooms.map(roomID => (
+          <li key={roomID}>
+            {roomID}
+            <button onClick={() => {
+              history(`/room/${roomID}`)
+            }}>JOIN ROOM</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => {
+        history(`/room/${v4()}`)
+      }}>Create New Room</button>
     </div>
   )
 }
