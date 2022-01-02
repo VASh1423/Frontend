18703ca00d5cd20a63e8280826187c0097419078
VASh <shokarev.vl@gmail.com>
2022-01-02 18:25:01 +0300

    updated

--- react-webrtc-websockets/src/pages/Main/Main.js
+++ react-webrtc-websockets/src/pages/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import socket from '../../socket'
 import ACTIONS from '../../socket/actions'
 import { useNavigate } from 'react-router-dom'
@@ -7,15 +7,18 @@ import { v4 } from 'uuid'
 export default function Main() {
   const history = useNavigate()
   const [rooms, updateRooms] = useState([])
+  const rootNode = useRef()
 
   useEffect(() => {
     socket.on(ACTIONS.SHARE_ROOMS, ({rooms = []}) => {
-      updateRooms(rooms)
+      if(rootNode.current){
+        updateRooms(rooms)
+      }
     })
   }, [])
 
   return (
-    <div>
+    <div ref={rootNode}>
       <h1>
         Available Rooms
       </h1>
--- react-webrtc-websockets/src/pages/Room/Room.js
+++ react-webrtc-websockets/src/pages/Room/Room.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useParams } from 'react-router'
+import useWebRTC, { LOCAL_VIDEO } from '../../hooks/useWebRTC'
 
 export default function Room() {
+  const {id: roomID} = useParams()
+  const {clients, provideMediaRef} = useWebRTC(roomID)
+
+  console.log(clients)
+
   return (
     <div>
-      Room
+      {clients.map((clientID) => {
+        return (
+          <div key={clientID}>
+            <video
+              ref={instance => {
+                provideMediaRef(clientID, instance)
+              }}
+              autoPlay
+              playsInline
+              muted={clientID === LOCAL_VIDEO}
+            />
+          </div>
+        )
+      })}
     </div>
   )
 }
