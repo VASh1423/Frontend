3bacf7532bba025586e767324139f09fdf0491c2
VASh <shokarev.vl@gmail.com>
2022-03-14 21:45:52 +0300

    updated

--- react-testing/src/pages/MainPage.jsx
+++ react-testing/src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Counter from '../components/Counter/Counter'
 
 export default function MainPage() {
   return (
     <div data-testid='main-page'>
       Main Page
+      <Counter/>
     </div>
   )
 }
