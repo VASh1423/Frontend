ec09fb809fe9053ba82cbcec837b380e4aefd7d5
VASh <shokarev.vl@gmail.com>
2022-04-07 11:55:58 +0300

    updated

--- react-map/frontend/src/App.js
+++ react-map/frontend/src/App.js
@@ -3,8 +3,42 @@ import RoomIcon from '@mui/icons-material/Room';
 import StarIcon from '@mui/icons-material/Star';
 import "mapbox-gl/dist/mapbox-gl.css";
 import './app.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import {format} from 'timeago.js'
 
 function App() {
+  const currentUser = "jane"
+  const [pins, setPins] = useState([])
+  const [currentPlaceId, setCurrentPlaceId] = useState(null)
+  const [newPlace, setNewPlace] = useState(null)
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get('/pins')
+        setPins(res.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getPins()
+  }, [])
+
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id)
+  }
+
+  const handleAddClick = (e) => {
+    const lat = e.lngLat.lat
+    const long = e.lngLat.lng
+    setNewPlace({
+      lat,
+      long
+    })
+  }
+
   return (
     <div className="App">
       <Map
@@ -16,30 +50,57 @@ function App() {
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
         style={{width: '100vw', height: '100vh'}}
         mapStyle="mapbox://styles/mapbox/streets-v9"
+        onDblClick={handleAddClick}
       >
-        <Marker longitude={2.294694} latitude={48.858093} >
-          <RoomIcon style={{color: 'slateblue'}}/>
-        </Marker>
-        <Popup longitude={2.294694} latitude={48.858093}
-          anchor="left">
-          <div className='card'>
-            <label>Place</label>
-            <h4 className='place'>The Eiffel Tower</h4>
-            <label>Review</label>
-            <p className='desc'>Beautiful place. I like it.</p>
-            <label>Rating</label>
-            <div className='stars'>
-              <StarIcon className='star'/>
-              <StarIcon className='star'/>
-              <StarIcon className='star'/>
-              <StarIcon className='star'/>
-              <StarIcon className='star'/>
+        {pins.map(p => (
+          <>
+            <Marker longitude={p.long} latitude={p.lat} >
+              <RoomIcon style={{color: p.username === currentUser ? 'tomato' : 'slateblue', cursor: 'pointer'}} onClick={() => handleMarkerClick(p._id)}/>
+            </Marker>
+            {p._id !== currentPlaceId &&
+              <Popup longitude={p.long} latitude={p.lat} anchor="left" onClose={() => setCurrentPlaceId(null)}>
+                <div className='card'>
+                  <label>Place</label>
+                  <h4 className='place'>{p.title}</h4>
+                  <label>Review</label>
+                  <p className='desc'>{p.desc}.</p>
+                  <label>Rating</label>
+                  <div className='stars'>
+                    <StarIcon className='star'/>
+                    <StarIcon className='star'/>
+                    <StarIcon className='star'/>
+                    <StarIcon className='star'/>
+                    <StarIcon className='star'/>
+                  </div>
+                  <label>Information</label>
+                  <span className='username'>Created by <b>{p.username}</b></span>
+                  <span className='date'>{format(p.createdAt)}</span>
+                </div>
+              </Popup>
+            }
+          </>
+        ))}
+        {newPlace && (
+          <Popup longitude={newPlace.long} latitude={newPlace.lat} anchor="left">
+            <div>
+              <form>
+                <label>Title</label>
+                <input placeholder='enter a title'/>
+                <label>Review</label>
+                <textarea placeholder='about'/>
+                <label>Rating</label>
+                <select>
+                  <option value='1'>1</option>
+                  <option value='2'>2</option>
+                  <option value='3'>3</option>
+                  <option value='4'>4</option>
+                  <option value='5'>5</option>
+                </select>
+                <button className='submitBtn' type='submit'>Add pin</button>
+              </form>
             </div>
-            <label>Information</label>
-            <span className='username'>Created by <b>VASh</b></span>
-            <span className='date'>1 hour ago</span>
-          </div>
-        </Popup>
+          </Popup>
+        )}
       </Map>
     </div>
   );
