7989bc157dd5bac33f36765c8c89ee6a962a7a45
VASh <shokarev.vl@gmail.com>
2022-04-11 10:59:12 +0300

    updated

--- react-quiz/src/components/Quiz.jsx
+++ react-quiz/src/components/Quiz.jsx
@@ -1,17 +1,50 @@
 import React, { useEffect, useState } from 'react'
+import useSound from 'use-sound'
+import play from '../assets/play.mp3'
+import correct from '../assets/correct.mp3'
+import wrong from '../assets/wrong.mp3'
 
-export default function Quiz({data, setTimeOut, questionNumber, setQuestionNumber}) {
+export default function Quiz({data, setStop, questionNumber, setQuestionNumber}) {
   const [question, setQuestion] = useState(null)
   const [selectedAnswer, setSelectedAnswer] = useState(null)
   const [className, setClassNamer] = useState('answer')
+
+  const [letsPlay] = useSound(play)
+  const [correctAnswer] = useSound(correct)
+  const [wrongAnswer] = useSound(wrong)
+
+  useEffect(() => {
+    letsPlay()
+  }, [letsPlay])
 
   useEffect(() => {
     setQuestion(data[questionNumber-1])
   }, [data, questionNumber])
 
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback()
+    }, duration)
+  }
+
   const handleClick = (a) => {
     setSelectedAnswer(a)
     setClassNamer('answer active')
+    delay(3000, () => setClassNamer(a.correct ? 'answer correct' : 'answer wrong'))
+    delay(5000, () => {
+      if(a.correct){
+        correctAnswer()
+        delay(1000, () => {
+          setQuestionNumber(prev => prev+1)
+          setSelectedAnswer(null)
+        })
+      }else{
+        wrongAnswer()
+        delay(1000, () => {
+          setStop(true)
+        })
+      }
+    })
   }
 
   return (
