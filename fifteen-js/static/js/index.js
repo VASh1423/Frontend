ebe35ce72dd2c9e25511dbca61c6775faf1751bf
VASh <shokarev.vl@gmail.com>
2022-04-17 11:55:30 +0300

    updated

--- fifteen-js/static/js/index.js
+++ fifteen-js/static/js/index.js
@@ -27,6 +27,47 @@ containerNode.addEventListener('click', event => {
   const buttonNumber = Number(buttonNode.dataset.matrixId)
   const buttonCords = findCoordinatesByNumbers(buttonNumber, matrix)
   const blankCords = findCoordinatesByNumbers(blankNumber, matrix)
+  const isValid = isValidForSwap(buttonCords, blankCords)
+
+  if(isValid){
+    swap(blankCords, buttonCords, matrix)
+    setPositionItems(matrix)
+  }
+})
+
+window.addEventListener('keydown', (event) => {
+  if(!event.key.includes('Arrow')){
+    return
+  }
+  const blankCords = findCoordinatesByNumbers(blankNumber, matrix)
+  const buttonCords = {
+    x: blankCords.x,
+    y: blankCords.y
+  }
+  const direction = event.key.split('Arrow')[1].toLowerCase()
+  const maxIndexMatrix = matrix.length
+
+  switch(direction){
+    case 'up':
+      buttonCords.y += 1
+      break
+    case 'down':
+      buttonCords.y -= 1
+      break
+    case 'left':
+      buttonCords.x += 1
+      break
+    case 'right':
+      buttonCords.x -= 1
+      break
+  }
+
+  if(buttonCords.y >= maxIndexMatrix || buttonCords.y < 0 || buttonCords.x >= maxIndexMatrix || buttonCords.x < 0){
+    return
+  }
+
+  swap(blankCords, buttonCords, matrix)
+  setPositionItems(matrix)
 })
 
 function getMatrix(arr){
@@ -78,4 +119,43 @@ function findCoordinatesByNumbers(number, matrix){
     }
   }
   return null
+}
+
+function isValidForSwap(coords1, coords2){
+  const diffX = Math.abs(coords1.x - coords2.x)
+  const diffY = Math.abs(coords1.y - coords2.y)
+
+  return (diffX === 1 || diffY === 1) && (coords1.x === coords2.x || coords1.y === coords2.y)
+}
+
+function swap(coords1, coords2, matrix){
+  const coordNumber = matrix[coords1.y][coords1.x]
+  matrix[coords1.y][coords1.x] = matrix[coords2.y][coords2.x]
+  matrix[coords2.y][coords2.x] = coordNumber
+
+  if(isWon(matrix)){
+    addWonClass()
+  }
+}
+
+const winArray = new Array(16).fill(0).map((_item, index) => index + 1)
+function isWon(matrix){
+  const flatMatrix = matrix.flat()
+  for(let i = 0; i < winArray.length; i++){
+    if(winArray[i] < flatMatrix[i]){
+      return false
+    }
+  }
+  return true
+}
+
+const wonClass = 'fifteenWon'
+function addWonClass(){
+  setTimeout(() => {
+    containerNode.classList.add(wonClass)
+
+    setTimeout(() => {
+      containerNode.classList.remove(wonClass)
+    }, 1000)
+  }, 200)
 }
