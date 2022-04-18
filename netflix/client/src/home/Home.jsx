7a6bac143fabd4f4c096e2c1b79f363a6bfaa616
VASh <shokarev.vl@gmail.com>
2022-04-18 16:28:54 +0300

    updated

---
+++ netflix/client/src/home/Home.jsx
@@ -0,0 +1,101 @@
+import { Navbar } from '../components/navbar/Navbar'
+import './home.scss'
+
+export const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar/>
+      <img
+        width='100%'
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quod nemo laudantium itaque nulla natus perspiciatis animi deserunt ad sint architecto laborum quam, officiis deleniti ratione, ab eligendi voluptas!
+    </div>
+  )
+}
