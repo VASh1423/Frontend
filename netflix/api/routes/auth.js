1e2d9896ac4e50f0873c02bf8740bc570138865b
VASh <shokarev.vl@gmail.com>
2022-04-29 19:00:02 +0300

    updated

--- netflix/api/routes/auth.js
+++ netflix/api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/User')
-const CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js")
+const jwt = require('jsonwebtoken')
 
 // REGISTER
 router.post('/register', async (req, res) => {
@@ -12,6 +13,27 @@ router.post('/register', async (req, res) => {
   try {
     const user = await newUser.save()
     return res.status(200).json(user)
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email})
+    !user && res.status(401).json('Wrong password or username')
+
+    const bytes  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password && res.status(401).json('Wrong password or username')
+
+    const accessToken = jwt.sign({userId: user._id, isAdmin: user.isAdmin}, process.env.SECRET_KEY, {expiresIn: '5d'})
+
+    const { password, ...info} = user._doc
+
+    return res.status(200).json({...info, accessToken})
   } catch (error) {
     return res.status(500).json(error)
   }
---
+++ netflix/api/routes/users.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const User = require('../models/User')
+const CryptoJS = require("crypto-js")
+
+// UPDATE
+router.put('/:id', async (req, res) => {
+  if(req.user.id === req.params.id || req.user.isAdmin){
+    
+  }
+})
+// DELETE
+// GET
+// GET ALL
+// GET USER STATISTIC
