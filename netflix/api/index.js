6421e69589e3e6451505a78797e695a180785f97
VASh <shokarev.vl@gmail.com>
2022-05-02 13:23:28 +0300

    updated

--- netflix/api/index.js
+++ netflix/api/index.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const authRoute = require('./routes/auth')
+const usersRoute = require('./routes/users')
+const movieRoute = require('./routes/movies')
 
 dotenv.config()
 
@@ -13,6 +15,8 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 
 app.use('/api/auth', authRoute)
+app.use('/api/users', usersRoute)
+app.use('/api/movies', movieRoute)
 
 app.listen(8800, () => {
   console.log('Server is running');
--- netflix/api/routes/users.js
+++ netflix/api/routes/users.js
@@ -1,14 +1,104 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const CryptoJS = require("crypto-js")
+const verify = require('../verifyToken')
 
 // UPDATE
-router.put('/:id', async (req, res) => {
-  if(req.user.id === req.params.id || req.user.isAdmin){
-    
+router.put('/:id', verify, async (req, res) => {
+  if(req.user.userId === req.params.id || req.user.isAdmin){
+    if(req.body.password){
+      req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
+    }
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
+      return res.status(200).json(updatedUser)
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+  } else {
+    return res.status(403).json('You can update only your account')
   }
 })
+
 // DELETE
+router.delete('/:id', verify, async (req, res) => {
+  if(req.user.userId === req.params.id || req.user.isAdmin){
+    try {
+      await User.findByIdAndDelete(req.params.id)
+      return res.status(200).json('User has been deleted')
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+  } else {
+    return res.status(403).json('You can delete only your account')
+  }
+})
+
 // GET
+router.get('/find/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const {password, ...info} = user._doc
+    return res.status(200).json(info)
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
 // GET ALL
+router.get('/', verify, async (req, res) => {
+  const query = req.query.new
+  if(req.user.isAdmin){
+    try {
+      const users = query ? await User.find().sort({_id: -1}).limit(10) : await User.find()
+      return res.status(200).json(users)
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+  } else {
+    return res.status(403).json('You are not allowed to see all users')
+  }
+})
+
 // GET USER STATISTIC
+router.get('/stats', async (req, res) => {
+  const today = new Date()
+  const lastYear = today.setFullYear(today.setFullYear()-1)
+
+  const monthsArray = [
+    'jan',
+    'fed',
+    'mar',
+    'apr',
+    'may',
+    'jun',
+    'jul',
+    'aug',
+    'sep',
+    'oct',
+    'nov',
+    'dec'
+  ]
+
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: {$month: '$createdAt'}
+        }
+      },
+      {
+        $group: {
+          _id: '$month',
+          total: {$sum: 1}
+        }
+      }
+    ])
+    return res.status(200).json(data)
+  } catch (error) {
+    return res.status(500).json(err)
+  }
+})
+
+module.exports = router
